e4634f12b88759f34a3320e53ef5e6fc9ddc2523
zaydm <122492735+Zaydiscool777@users.noreply.github.com>
2024-04-03 17:17:58 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+# import math
+
+print('beginning software...')
+print('defining global functions...')
+
+def lerp(given, goal): # small general partition of the future reinforcement function
+	return (given*.5)+(goal*.5)
+"""sig = lambda x: 1 / (1 + math.exp(-x))
+isig = lambda x: -math.log(() - 1)"""
+aid = lambda x: x # aid for activator id because id already has a function
+iaid = lambda x: x
+
+print('getting classy...')
+
+class Returner: # to allow input and output of a neuron
+	def __init__(self, value=0):
+		self.r = value
+	def __call__(self):
+		return self.r
+
+class Neuron(Returner):
+	def __init__(self, cons: list):
+		super().__init__()
+		self.cons = cons # input
+		self.w = [1] * len(cons)
+		self.b = 0
+	def comp(self):
+		x = 0
+		for i in enumerate(self.cons):
+			x += i[1]() * self.w[i[0]]
+		x += self.b
+		self.r = aid(x)
+		return self.r
+
+print('evaluating tests...')
+
+ai = []
+ai.append([Returner(1), Returner(2), Returner(3)])
+ai.append(Neuron(ai[0]))
+ai[1].w = [1/3] * 3
+
+print('test 1: use pre-made perceptron: expected: 2.0; actual: ' + str(ai[1].comp()))
